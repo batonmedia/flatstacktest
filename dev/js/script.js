e3e451716771e316424c7886bc8e8a3220dc2a37
Vlad Krasnoperov <batonmail@gmail.com>
2015-08-08 01:50:49 +0300

    Jade template & JS file added

---
+++ dev/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var headerNav = $('.header-nav');
+
+    //$(window)
+    //    .on("scroll", function () {
+    //        $(window).scrollTop() > 40 ? headerNav.addClass('header-nav-fixed') : headerNav.removeClass('header-nav-fixed');
+    //    })
+    //    .trigger("scroll");
+});
